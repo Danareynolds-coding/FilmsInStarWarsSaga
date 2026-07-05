381f08032aec994751438e07ad6b79d9c3a0775d
Danareynolds-coding <dana.reynolds@mscoding.org>
2026-07-05 18:22:46 -0400

    changes

--- README.md
+++ README.md
@@ -1,18 +1,25 @@
 # FilmsInStarWarsSaga
 App to display Star Wars Film Information on Cards
 
-Requirements: Basic HTML, CSS and JS (I wanted to make sure I had something to turn if more ambitious project ran into problems and I was unable to meet deadline.)
-#1 Public GitHub
+####Requirements: Basic HTML, CSS and JS (I wanted to make sure I had something to turn if more ambitious project ran into problems and I was unable to meet deadline.)
+##CONTENT:
+###1 Public GitHub
   https://github.com/Danareynolds-coding/FilmsInStarWarsSaga
-#2 An HTML document for the page.
+###2 An HTML document for the page.
   HTML for each: home, films, people and species, planets and spaceships and vehicles
-#3 A CSS document to style the HTML page.
+###3 A CSS document to style the HTML page.
   CSS called "styles.css"
-#4 A JavaScript file that retrieves data from public API sources to display the data on your HTML page.
+###4 A JavaScript file that retrieves data from public API source to display the data on your HTML page.
   JS file called "app.js" with API from called "SWAPI The Star Wars API".
-#5 A README file that includes the instructions for running the webpage.
+###5 A README file that includes the instructions for running the webpage.
   Instructions for running web page below.
+##FUNCTIONALITY:
+####1 Code runs without issues and like instructions
+####2 Navigation between end points work
+####3 Code readable and structured
+####4 handle errors
+####5 styling ok?
 
 ##Instructions for User:
-  ###Website is siple and self explanitory.  There is a navigation bar at the top of each html sheet.  
-  simply click on the info page you want
+  ###Website is simple and self explanitory.  There is a navigation bar at the top of each html sheet.  
+  simply click on the info page you want. There you will information pulled from the API based on labeled subject.
--- app.js
+++ app.js
@@ -1,38 +1,36 @@
 
-let filmUrl = 'https://swapi.dev/api/films/1/'
+let filmUrl = 'https://swapi.dev/api/films'
 let film = document.getElementById('film')
 fetch(filmUrl).then(response => response.json()).then(json => {
   console.log(json)
-  film.innerHTML =`<h3>${json.title}   Episode: ${json.episode_id} </h3><p>${json.director}</p> <p> ${json.opening_crawl}`
-})
+  film.innerHTML =`<h3>${json.title}   Episode: ${json.episode_id} </h3><p>${json.director}</p> <p> ${json.opening_crawl}`}).catch(err => console.error(err))
+  
 
-let peopleUrl = 'https://swapi.dev/api/people/1/'
+
+let peopleUrl = 'https://swapi.dev/api/people/'
 let people = document.getElementById('people')
 fetch(peopleUrl).then(response => response.json()).then(json => {
   console.log(json)
   people.innerHTML =`<h3>${json.name} </h3><p>Birth Year ${json.birth_year},  Gender:${json.gender} 
   ${json.homeworld}:${json.species}</p>FILMS:${json.films}<p>
-  `
-})
+    `}).catch(err => console.error(err))
 
-let planetsUrl = 'https://swapi.dev/api/planets/1/'
+let planetsUrl = 'https://swapi.dev/api/planets/'
 let planets = document.getElementById('planets')
 fetch(planetsUrl).then(response => response.json()).then(json => {
   console.log(json)
   planets.innerHTML =`<h3>${json.name} </h3><p>population : ${json.population}, climate:${json.climate} ${json.terrain}</p><p>Days:${json.orbital_period}-   Hours per day ${json.rotation_period}</p>Residents:${json.residents}</p>
-  `
-})
+  `}).catch(err => console.error(err))
 
-let speciesUrl = 'https://swapi.dev/api/species/1/'
+let speciesUrl = 'https://swapi.dev/api/species/'
 let species = document.getElementById('species')
 fetch(speciesUrl).then(response => response.json()).then(json => {
   console.log(json)
   species.innerHTML =`<h3>${json.name} </h3><p>classification : ${json.classification}, 
-     designation:${json.designation}</p>
-  `
-})
+     designation:${json.designation}</p>`
+}).catch(err => console.error(err))
 
-let starshipsUrl = 'https://swapi.dev/api/starships/1/'
+let starshipsUrl = 'https://swapi.dev/api/starships/'
 let starships = document.getElementById('starships')
 fetch(starshipsUrl).then(response => response.json()).then(json => {
   console.log(json)
@@ -40,13 +38,12 @@ fetch(starshipsUrl).then(response => response.json()).then(json => {
   </p Pilots: ${json.pilots}  Crew:${json.crew}   ${json.max_atmosphering_speed}</p>
    
   `
-})
+}).catch(err => console.error(err))
 
-let vehiclesUrl = 'https://swapi.dev/api/vehicles/1/'
+let vehiclesUrl = 'https://swapi.dev/api/vehicles/'
 let vehicles = document.getElementById('vehicles')
 fetch(vehiclesUrl).then(response => response.json()).then(json => {
-  console.log(json)
+  console.log(json) 
   vehicles.innerHTML =`<h3>${json.name}  Model:${json.model}</h3>
-  <p>Pilots: ${json.pilots}    Crew:${json.crew}  Vehicle Class: ${json.vehicle_class}  Speed:${json.max_atmosphering_speed}</p>
-  `
-})
+  <p>Pilots: ${json.pilots}    Crew:${json.crew}  Vehicle Class: ${json.vehicle_class}  Speed:${json.max_atmosphering_speed}</p>`
+}).catch(err => console.error(err))
